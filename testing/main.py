daeba4ed4cdae67de29b4700c0c69d60a8482549
Alejandro <alexrios182@gmail.com>
2022-10-11 20:25:27 -0500

    add an import/export example

--- testing/main.py
+++ testing/main.py
@@ -1,10 +1,19 @@
 import petl as etl
+
+# Toma el archivo y lo transforma en un formato imprimible
 csvDatabase = etl.fromcsv('./resources/data.csv')
 
-# imprimir filas de la tabla
-print(csvDatabase.head(2))
+# imprime el contenido del archivo csv
+print(csvDatabase)
 
+# creacion de un nuevo set de datos conteniendo las columnas nombre y edad
 tabla2 = [['nombre','edad'],['jorge', 20],['maria', 30],['jose', 40],['jose', 50]]
-etl.tocsv(tabla2, 'misdatos.csv')
 
-print(tabla2.head(1))
+# exportamos el contenido del nuevo set de datos a un archivo csv
+etl.tocsv(tabla2, './resources/newData.csv')
+
+# abrimos el archivo recien creado y guardamos su contenido en una nueva variable
+newDatabase = etl.fromcsv('./resources/newData.csv')
+
+# imprimimos el contenido de nuestra segunda tabla
+print(newDatabase)
