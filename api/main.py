1976509f635b706f83a52c296d8a9e5053479c0a
Alejandro <alexrios182@gmail.com>
2022-12-01 20:19:40 -0600

    agregar nuevos endpoints para graficas

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 # Connect to the database
 connection = mysql.connector.connect(
-    user='root', password='', host='localhost', database='nuevas_escuelas')
+    user='root', password='admin', host='localhost', database='dwh')
 # Retrive the cursor
 cursor = connection.cursor()
 
@@ -23,6 +23,74 @@ def getResults(cursor):
 def root():
     return "HOME"
 
+# -------------------------------------------------------
+# Escuelas por colonia
+@app.get("/escuelas-por-colinia/")
+def escuelas_por_colonia():
+    consulta = """SELECT COUNT(id_colonia), id_escuela 
+                    FROM colonia_escuela
+                    GROUP BY id_escuela 
+                    LIMIT 10;"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+
+# Negocios por colonia
+@app.get("/negocios-por-colinia/")
+def negocios_por_colonia():
+    consulta = """SELECT COUNT(id_colonia), id_negocio 
+                    FROM colonia_negocio
+                    LIMIT 10;"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+
+# Escuelas con mayor numero de estudiantes del estado del jalisco
+@app.get("/escuelas-num-estudiantes/")
+def escuelas_num_estudiantes():
+    consulta = """SELECT nombre, ciudad, numero_alumnos 
+                    FROM dim_escuela
+                    ORDER BY numero_alumnos desc
+                    LIMIT 10;"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+
+# Numero de alumnos en las escuelas segun su nivel academico en guadalajara
+@app.get("/nivel-escuelas/")
+def nivel_escuelas():
+    consulta = """SELECT sum(numero_alumnos) AS num_alumnos, ciudad, nivel 
+                    FROM dim_escuela
+                    WHERE ciudad='GUADALAJARA' AND numero_alumnos != 0
+                    GROUP BY nivel;"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+
+# Porcentaje de escuelas de nivel publico contra nivel privado en el estado de jalisco
+# Para esto se necesitan hacer los resultados de las dos consultas para poder sacar el porcentaje
+# TOTAL = 15901 (100%)
+
+# 12266 (77.2%)
+@app.get("/numero-escuelas-publicas/")
+def numero_escuelas_publicas():
+    consulta = """SELECT count(*) 
+                    FROM dim_escuela
+                    WHERE control='PUBLICO';"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+
+#3635 (22.8%)
+@app.get("/numero-escuelas-privadas/")
+def numero_escuelas_privadas():
+    consulta = """SELECT count(*) 
+                    FROM dim_escuela
+                    WHERE control='PRIVADO';"""
+    cursor.execute(consulta)
+    resultado = cursor.fetchall()
+    return resultado
+# -------------------------------------------------------
 
 @app.get("/colonia_negocios/{colonia}")
 def colonia_negocios(colonia: str):
